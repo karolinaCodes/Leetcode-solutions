28422ebf9f7407795e1d88c6585e0e3e8bd068bd
Karolina <82968631+karolinaCodes@users.noreply.github.com>
2022-01-05 21:13:40 -0500

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -18,6 +18,7 @@ var twoSum = function (nums, target) {
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
-console.log(twoSum([3, 3], 6));
+// DRIVER CODE
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
