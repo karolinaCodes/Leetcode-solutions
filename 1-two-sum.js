d397ef149946a660759a48395298c614ab4a665b
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-14 15:50:18 -0500

    add driver code comment and change var to const

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,9 +1,9 @@
-/* Intructions: 
+/* Description: 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.*/
 
-var twoSum = function (nums, target) {
+const twoSum = function (nums, target) {
   // create clone to reset the value of the array to original array
   let numsClone = [...nums];
 
@@ -23,6 +23,7 @@ var twoSum = function (nums, target) {
   }
 };
 
+// Driver code //
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
